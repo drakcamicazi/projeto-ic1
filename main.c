06c571d66a7dc62479c938d9410f9c49921ccb7d
Unknown <drak.milena@gmail.com>
2019-06-21 05:41:55 -0300

    alguem me mata pfv

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_LINHAS 29
-#define TOTAL_CHARS 90900
+#define TOTAL_CHARS 100000
 
 struct filme{
 	char title[100];
@@ -24,16 +24,15 @@ struct structAtributo{
 typedef struct filme Filme;
 typedef struct structAtributo Atributo;
 
-//void armazenaAtributo(int aFilme, int aTipo, char[100] aDesc){}
-
 int main(){
 	FILE *netflix, *processado;
 	Filme filmes[MAX_LINHAS];
-	Atributo a[203]; //203 é 29 * 7  (num de filmes * num de atributos por filme)
-	char linha[255], desc[100][203], *token, netflixStr[TOTAL_CHARS], c;
-	int i = 0, primeiro = 1, atributo = 1, idFilme = 0, linhai = 2;
-	int tamanhoNetflixStr;
-
+	Atributo a[7 * MAX_LINHAS];
+	char linha[255], *token, netflixStr[TOTAL_CHARS], c;
+	int i = 0, primeiro = 1, atributo = 1, idFilme = 0, linhai = 2, tamanhoNetflixStr;
+	int l=0, lFilme=0, lTipo=0, pontovirgula = -1;
+	char lDesc[100]="";
+	//------------PARTE 0: PRÉ-PROCESSAMENTO DO ARQUIVO NETFLIX_ALL.CSV---------------
 	processado = fopen("netflix_preproc.txt", "w");
 	netflix = fopen("netflix_30.csv", "r");
 
@@ -43,7 +42,6 @@ int main(){
 
 	for(i=0; i < tamanhoNetflixStr; i++){ //FOR PARA PRÉ-PROCESSAMENTO DO ARQUIVO
 		c = netflixStr[i];
-
 		if (c == ';') { //quando encontrar um separador
 			if (idFilme != 0) fprintf(processado, "\n%i;%i;%i;", idFilme-1, atributo, linhai-8);
 			if (atributo == 0){
@@ -58,23 +56,34 @@ int main(){
 		else
 		if (c == '\n') { //quando pula linha e o idFilme tá dentro do limite
 			if (idFilme != 0)
-				if (linhai != 9) fprintf(processado, "\n%i;%i;%i;", idFilme-1, atributo, linhai-8);
-				else fprintf(processado, "%i;%i;%i;", idFilme-1, atributo, linhai-8);
+			if (linhai != 9) fprintf(processado, "\n%i;%i;%i;", idFilme-1, atributo, linhai-8);
+			else fprintf(processado, "%i;%i;%i;", idFilme-1, atributo, linhai-8);
 
 			linhai++;
 		}
 		else { //quando encontrar qualquer outro caractere
 			if (idFilme != 0) fprintf(processado, "%c", c);
 		}
-
 		atributo = linhai % 8;
-
 	}
 
 	fclose(netflix);
+	fclose(processado);
 
+	//--------------PARTE 1: ARMAZENAR EM REGISTROS DO TIPO FILME---------------
+
+	processado = fopen("netflix_preproc.txt", "r");
+
+	while ((fscanf(processado, "%[^\n]", &linha )) != EOF) {
+		puts(linha); //POR QUE VC CAI EM UM LOOP INFINITO???----------------------------
+
+	}
+	// while ((c = fgetc(processado)) != EOF) {
+	// 	printf("%c", c);
+	// 	if (c == ';')	pontovirgula++;
+	// 	else pontovirgula = -1;
+	// }
 	fclose(processado);
 
 	return 0;
-
 }

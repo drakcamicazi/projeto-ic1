7b39e9bff87efebfd8920750465ed2f9e78caebe
Unknown <drak.milena@gmail.com>
2019-06-21 02:15:33 -0300

    ainda preciso remover a primeira linha

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 //Thalles Raphael Guimarães, nº USP 11320297
 #include <stdio.h>
 #include <string.h>
-#define MAX_LINHAS 30
+#define MAX_LINHAS 29
 #define TOTAL_CHARS 90900
 
 struct filme{
@@ -16,13 +16,21 @@ struct filme{
 	int urSize;
 };
 
-typedef struct filme Filme;
+struct structAtributo{
+	int aFilme, aTipo;
+	char aDesc[100];
+};
 
+typedef struct filme Filme;
+typedef struct structAtributo Atributo;
+
+//void armazenaAtributo(int aFilme, int aTipo, char[100] aDesc){}
 
 int main(){
 	FILE *netflix;
 	Filme filmes[MAX_LINHAS];
-	char linha[255], *token, netflixStr[TOTAL_CHARS], c;
+	Atributo a[203]; //203 é 29 * 7  (num de filmes * num de atributos por filme)
+	char linha[255], desc[100][203], *token, netflixStr[TOTAL_CHARS], c;
 	int i = 0, primeiro = 1, atributo = 1, idFilme = 0, linhai = 1;
 	int tamanhoNetflixStr;
 
@@ -31,22 +39,27 @@ int main(){
 	//lê o arquivo inteiro e armazena na string netflixStr
 
 	tamanhoNetflixStr = fread(&netflixStr, sizeof(*netflixStr), TOTAL_CHARS, netflix);
-	printf("%i.%i.%i- ", idFilme, atributo, linhai);
+	printf("%i.%i.%i.", idFilme, atributo, linhai);
 	linhai++;
 	for(i=0; i < tamanhoNetflixStr; i++){
 		c = netflixStr[i];
 
 		if (c == ';') { //quando encontrar um separador
-			printf("\n%i.%i.%i- ", idFilme, atributo, linhai);
+			printf("\n%i.%i.%i.", idFilme, atributo, linhai);
+
 			if (atributo == 0){
-				idFilme = idFilme + 1;
 				atributo = 1;
+				idFilme = idFilme + 1;
+				if (idFilme > MAX_LINHAS) {
+					printf("\nFim.\n");
+					break; //break o for quando atinge o máximo de filmes
+				}
 			}
 			linhai++;
 		}
 		else
-		if (c == '\n') { //quando pula linha
-			printf("---- Fim de filme\n\n%i.%i.%i- ", idFilme, atributo, linhai);
+		if (c == '\n') { //quando pula linha e o idFilme tá dentro do limite
+			printf("\n\n%i.%i.%i.", idFilme, atributo, linhai);
 			linhai++;
 		}
 		else { //quando encontrar qualquer outro caractere
